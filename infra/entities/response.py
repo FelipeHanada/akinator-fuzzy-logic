9ad383c88ba211450d06c4122c6dbfcc8b2355d3
Felipe Hanada <felipe.eizohanada@gmail.com>
2023-11-22 22:14:44 -0300

    feat: add fuzzy system

--- infra/entities/response.py
+++ infra/entities/response.py
@@ -10,6 +10,7 @@ class Response(Base):
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
 
     response = Column(Integer, nullable=False)
+    # this is an integer from 0 to 1 (no/yes)
 
     person_id = Column(String, ForeignKey("person_table.id"))
     person = relationship("Person", back_populates="responses")
