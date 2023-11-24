b8c744569e1a2c8a662d6b6fa08fdb35d14769a9
Felipe Hanada <felipe.eizohanada@gmail.com>
2023-11-24 05:53:25 -0300

    add: app multi page system

---
+++ pages/guess_page.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+from pages.page import Page
+
+class GuessPage(Page):
+    def __init__(self, container, controller):
+        super().__init__(container, controller)
+
+    def create_widgets(self):
+        pass
